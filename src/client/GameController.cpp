5946bc32b1067a7245c399b115254bcc776d5dae
jzfrank <frankjinzhang@gmail.com>
2022-05-13 20:42:28 +0200

    added external library sockpp and rapidjson

--- src/client/GameController.cpp
+++ src/client/GameController.cpp
@@ -64,7 +64,7 @@ void GameController::flipCard(int row, int col) {
         std::cout << "next player" << std::endl;
 
         // Show message that current turn is finished
-        // TODO: add timeout 
+        // TODO: add timeout
         std::string message = "Your turn is finished";
         std::string title = "Message";
         wxMessageDialog dialogBox = wxMessageDialog(nullptr, message, title, wxICON_NONE);
